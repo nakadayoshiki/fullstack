756e0d4a4e8cd8f3cac81749a121ff4f52d30d43
nakadayoshiki <y.nakada.rkt@gmail.com>
2020-06-20 16:01:13 +0900

    Create home_controller

---
+++ api/controllers/home_controller.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/nakadayoshiki/fullstack/github.com/nakadayoshiki/fullstack/api/responses"
+)
+
+func (s *Server) Home(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, "Welcomu To This Awesome API")
+}
