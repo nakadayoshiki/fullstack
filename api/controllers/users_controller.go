8e7df672e93ab41b18c7661605b70fc695dbe9a1
nakadayoshiki <y.nakada.rkt@gmail.com>
2020-06-20 18:39:21 +0900

    Create DeleteUser

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -114,3 +114,32 @@ func (s *Server) UpdatedUser(w http.ResponseWriter, r *http.Request) {
 	}
 	responses.JSON(w, http.StatusOK, updatedUser)
 }
+
+func (s *Server) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user := models.User{}
+
+	uid, err := strconv.ParseUint(vars["uid"], 10, 32)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+	if tokenID != 0 && tokenID != uint32(uid) {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+
+	_, err = user.DeleteAUser(s.DB, uint32(uid))
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	w.Header().Set("Entity", fmt.Sprintf("%d", uid))
+	responses.JSON(w, http.StatusNoContent, "")
+}
