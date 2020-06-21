e00dc224cb999b8cba8df49a9e70e82c68ec890b
nakadayoshiki <y.nakada.rkt@gmail.com>
2020-06-21 12:23:20 +0900

    mysql connect success!!

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/nakadayoshiki/fullstack/github.com/nakadayoshiki/fullstack/api/models"
 )
 
